2e5b873a6c9d5f215975a205d5f89d0406d51e67
Andreas Schmieder <andreas.schmieder@googlemail.com>
2020-03-30 22:24:36 +0200

    works with app and net

--- api-service/api/server.ts
+++ api-service/api/server.ts
@@ -19,40 +19,6 @@ app.use(compression());
 server.applyMiddleware({ app, path: '/graphql' });
 const httpServer = createServer(app);
 
-// import {Game} from "./entity/Game";
-// import Database from "./database";
-//
-// const names = ['yasin', 'julian', 'andi'];
-//
-// let game = new Game();
-// game.createNewGame(names);
-//
-// Database.getInstance().connect().then( async (connection: any) => {
-//     let gamesRepo = connection.getRepository(Game);
-//     let _game = await gamesRepo.findOne({gamers: game.gamers, archived: false});
-//     if (!_game) {
-//         await gamesRepo.save(game);
-//         console.log('game saved', game);
-//     } else {
-//         console.log('diese Spielerkonstellation gibts bereits');
-//     }
-//     _game.uppScore('andi');
-//     await gamesRepo.save(_game);
-//
-// });
-//
-
-// game.uppScore('Andi');
-// game.uppScore('Andi');
-// game.uppScore('Andi');
-// game.uppScore('Andi');
-
-
-
-
-
-
-
 httpServer.listen(
     { port: 3000},
     (): void => console.log(`\n🚀      GraphQL is now running on http://localhost:3000/graphql`));
